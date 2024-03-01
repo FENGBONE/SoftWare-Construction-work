60876056543748600b812235073080d4239b057a
FENGBONE <2426651109@QQ.com>
2024-03-01 18:24:02 +0800

    assignment1

--- assignment1/calculator1/Program.cs
+++ assignment1/calculator1/Program.cs
@@ -10,6 +10,30 @@ namespace calculator1
     {
         static void Main(string[] args)
         {
+            int a, b;
+            char x;
+            a=int.Parse(Console.ReadLine());
+            x=char.Parse(Console.ReadLine());
+            b=int.Parse(Console.ReadLine());
+            switch (x)
+            {
+                case '+':
+                    Console.WriteLine(a + b);
+                    break;
+                case '-':
+                    Console.WriteLine(a - b);
+                    break;
+                case '*':
+                    Console.WriteLine(a * b);
+                    break;
+                case '/':
+                    Console.WriteLine(a / b);
+                    break;
+                default: 
+                    Console.WriteLine("wrong");
+                    break;
+            }
+            Console.Read();
         }
     }
 }
