e5b2f7f32d7b4bc820659ec4b7ead5cd7a4ca017
FENGBONE <2426651109@QQ.com>
2024-03-01 13:18:12 +0800

    assignment1

---
+++ assignment1/calculator1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
