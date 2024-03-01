a44d33240b0feb7d1c48975ac4a5729b3b717650
FENGBONE <2426651109@QQ.com>
2024-03-01 19:55:53 +0800

    assignment1

---
+++ assignment1/calculator/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace calculator
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            int a, b;
+            char c;
+            while (true)
+            {
+
+            }
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Button_Click2(object sender, RoutedEventArgs e) { }
+        private void Button_Click3(object sender, RoutedEventArgs e) { }
+        private void Button_Click4(object sender, RoutedEventArgs e) { }
+        private void Button_Click5(object sender, RoutedEventArgs e) { }
+        private void Button_Click6(object sender, RoutedEventArgs e) { }
+        private void Button_Click7(object sender, RoutedEventArgs e) { }
+        private void Button_Click8(object sender, RoutedEventArgs e) { }
+        private void Button_Click9(object sender, RoutedEventArgs e) { }
+        private void Button_Click0(object sender, RoutedEventArgs e) { }
+        private void Button_ClickAdd(object sender, RoutedEventArgs e) { }
+        private void Button_ClickSub(object sender, RoutedEventArgs e) { }
+        private void Button_ClickMul(object sender, RoutedEventArgs e) { }
+        private void Button_ClickDiv(object sender, RoutedEventArgs e) { }
+        private void Button_ClickSure(object sender, RoutedEventArgs e) { }
+    }
+}
