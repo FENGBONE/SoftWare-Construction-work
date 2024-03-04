9e915b2e6076a4cd206388c1175f1c2401599dbe
FENGBONE <2426651109@QQ.com>
2024-03-04 19:23:23 +0800

    窗体应用计算器1.0（加减乘除）

---
+++ assignment1/calculator2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculator2
+{
+    public partial class Form1 : Form
+    {
+        int a;
+        int b;
+        int num;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //减法按钮点击事件
+        private void button5_Click(object sender, EventArgs e)
+        {
+            a = int.Parse(textBox1.Text);
+            b = int.Parse(textBox2.Text);
+            num = a - b;
+           // label1.Text = num.ToString();
+        }
+
+        //加法按钮点击事件
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a=int.Parse(textBox1.Text);
+            b=int.Parse(textBox2.Text);
+            num = a + b;
+           // label1.Text = num.ToString();
+        }
+
+        //乘法按钮点击事件
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a = int.Parse(textBox1.Text);
+            b = int.Parse(textBox2.Text);
+            num = a * b;
+           // label1.Text = num.ToString();
+        }
+        //除法按钮点击事件
+        private void button3_Click(object sender, EventArgs e)
+        {
+            a = int.Parse(textBox1.Text);
+            b = int.Parse(textBox2.Text);
+            num = a / b;
+            //label1.Text = num.ToString();
+        }
+
+        //等号按钮点击事件
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = num.ToString();
+        }
+    }
+}
